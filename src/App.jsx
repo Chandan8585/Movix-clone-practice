42d24d2e148fc8af6e1bd029785fe50a049565d2
Chandan8585 <chandanwingshr@gmail.com>
2023-05-15 22:41:59 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,14 @@ import Explore from './pages/explore/Explore'
 import { useEffect } from 'react'
 
 function App() {
-  useEffect(()=> {
-     apitesting()
-  }, [])
-  const apitesting = ()=> {
-       
-  }
+  // useEffect(()=> {
+  //    apitesting()
+  // }, [])
+  // const apitesting = ()=> {
+  //       fetch("https://api.themoviedb.org/3/movie/550?api_key=821acf172e6be981ab5d87f9c4b97ddd")
+  //                .then(res=>res.json())
+  //                .then(data=> console.log(data));  
+  // }
   return (
     <div>
    <BrowserRouter> 
@@ -24,7 +26,7 @@ function App() {
 <Routes>
   <Route path='/' element={<Home/>}/>
   <Route path='/:mediaType/:id' element={<Details/>}/>
-  <Route path='/expore/:mediaType' element={<Explore/>} />
+  <Route path='/explore/:mediaType' element={<Explore/>} />
   <Route path='/*' element={<PageNotFound/>} />
   <Route path='/search/:query' element={<SearchResults/>} />
 </Routes>
--- src/pages/search/SearchResults.jsx
+++ src/pages/search/SearchResults.jsx
@@ -3,7 +3,7 @@ import "./style.scss"
 const SearchResults = () => {
   return (
     <div>
-      
+     Search Page 
     </div>
   )
 }
